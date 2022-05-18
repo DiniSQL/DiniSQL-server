2dd37b5421924658bec823f5170e2b8be3af1956
OE-Heart <leoouyixin@126.com>
2022-05-18 21:44:09 +0800

    Fix Region register

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 	"DiniSQL/Region"
 	"bufio"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 	"time"
@@ -164,7 +166,34 @@ func runShell(r chan<- error) {
 // 	}
 // }
 
+func runEtcd() {
+	cmd := exec.Command("etcd", "--name", "infra1", "--initial-advertise-peer-urls", "http://192.168.84.244:2380",
+		"--listen-peer-urls http://192.168.84.244:2380",
+		"--listen-client-urls http://192.168.84.244:2379,http://127.0.0.1:2379",
+		"--advertise-client-urls http://192.168.84.244:2379",
+		"--initial-cluster-token etcd-cluster-1",
+		"--initial-cluster infra0=http://192.168.84.48:2380,infra1=http://192.168.84.244:2380",
+		"--initial-cluster-state new")
+	// cmd := exec.Command("etcd", "--version")
+	stout, err := cmd.StdoutPipe()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer stout.Close()
+	if err := cmd.Start(); err != nil {
+		log.Fatalln(err)
+	}
+
+	if opBytes, err := ioutil.ReadAll(stout); err != nil {
+		log.Fatalln(err)
+	} else {
+		fmt.Println(string(opBytes))
+	}
+
+}
+
 func main() {
+	// runEtcd()
 	var endpoints = []string{"127.0.0.1:2379"}
 	ser, err := Region.NewServiceRegister(endpoints, "/web/node1", "localhost:8000", 5)
 	if err != nil {
