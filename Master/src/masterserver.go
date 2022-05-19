ea41130e56eff8193ba35c0fbf127d5744a825a9
L1ght <2504082957@qq.com>
2022-05-19 20:49:11 +0800

    add CreatePacket and HandleClient

--- Master/src/masterserver.go
+++ Master/src/masterserver.go
@@ -29,11 +29,18 @@ const (
 
 var tab2reg map[string]*RegionStatus
 var id2reg map[int]*RegionStatus
+var regionCnt int
+var StatementChannel chan types.DStatements
+var FinishChannel chan string
+var TablesChannel chan []string
+
+//var StatementChannel = make(chan types.DStatements, 500)
 
 func initMaster() {
 	// Initialize the master server
 	tab2reg = make(map[string]*RegionStatus) // table name to region status
 	tab2reg["foo"] = new(RegionStatus)
+	regionCnt = 0
 	id2reg = make(map[int]*RegionStatus) // region id to region status
 	id2reg[0] = new(RegionStatus)
 	id2reg[0].regionID = 1
@@ -71,7 +78,7 @@ func ConnectToRegion(regionIP string, regionPort int, packet Type.Packet) (recPa
 	conn.Close()
 	fmt.Printf("send %d to %s\n", packet.Head.P_Type, conn.RemoteAddr())
 	// listen after send
-	recPacket = KeepListening(ClientIP, ClientPort)
+	recPacket = HandleClient(ClientIP, ClientPort)
 	return
 
 }
@@ -108,14 +115,44 @@ func ConnectToClient(regionIP string, regionPort int, packet Type.Packet) (recPa
 	conn.Close()
 	fmt.Printf("send %d to %s\n", packet.Head.P_Type, conn.RemoteAddr())
 	// listen after send
-	//recPacket = KeepListening(ClientIP, ClientPort)
+	//recPacket = HandleClient(ClientIP, ClientPort)
 	return
 
 }
 
-// listen
-// input : IP and Port of client
-func KeepListening(ClientIP string, ClientPort int) (receivedPacket Type.Packet) {
+func CreatePacket(statement types.DStatements, tables []string) Type.Packet {
+	packet := Type.Packet{}
+	packet.Head = Type.PacketHead{P_Type: Type.Answer, Op_Type: statement.GetOperationType(), Spare: ""}
+	var pay string
+	switch statement.GetOperationType() {
+	case types.CreateDatabase:
+	case types.UseDatabase:
+	case types.CreateTable:
+		//numRegions := min(3, regionCnt)
+
+	case types.CreateIndex:
+
+	case types.DropTable:
+
+	case types.DropIndex:
+
+	case types.DropDatabase:
+	case types.Insert:
+
+	case types.Update:
+
+	case types.Delete:
+
+	case types.Select:
+
+	case types.ExecFile:
+
+	}
+	packet.Payload = []byte(pay)
+	return packet
+}
+
+func HandleClient(ClientIP string, ClientPort int) (receivedPacket Type.Packet) {
 	fmt.Println("listening...")
 	listener, err := net.Listen("tcp", MASTER_PORT)
 	defer listener.Close()
@@ -127,7 +164,7 @@ func KeepListening(ClientIP string, ClientPort int) (receivedPacket Type.Packet)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("remote address:", conn.RemoteAddr())
+	fmt.Println("remote connected! address:", conn.RemoteAddr())
 	res, err1 := ioutil.ReadAll(conn)
 	if err1 != nil {
 		log.Println(err)
@@ -143,9 +180,23 @@ func KeepListening(ClientIP string, ClientPort int) (receivedPacket Type.Packet)
 	fmt.Printf("p.Head.P_Type:%d\n", receivedPacket.Head.P_Type)
 	fmt.Printf("p.Head.Op_Type:%d\n", receivedPacket.Head.Op_Type)
 	fmt.Printf("p.Payload:%s\n", receivedPacket.Payload)
-
-	//p := Type.Packet{Head: Type.PacketHead{P_Type: Type.KeepAlive, Op_Type: Type.CreateIndex},
-	//	Payload: []byte("foo")}
+	err = parser.Parse(strings.NewReader(string(receivedPacket.Payload)), StatementChannel) //收到客户端传过来的语句
+	if err != nil {
+		log.Fatal(err)
+	}
+	tabs := <-TablesChannel
+	statement := <-StatementChannel
+	fmt.Println("tabs:", tabs)
+	targetRegion := tab2reg[tabs[0]]
+	p := Type.Packet{Head: Type.PacketHead{P_Type: Type.Answer, Op_Type: statement.GetOperationType()},
+		Payload: []byte(targetRegion.regionIP + ":" + targetRegion.regionPort)}
+	packetBuf := make([]byte, 500)
+	packetBuf, err = p.MarshalMsg(packetBuf)
+	if err != nil {
+		log.Fatal(err) // Println + os.Exit(1)
+		return
+	}
+	_, err1 = conn.Write(packetBuf)
 	//fmt.Printf("remote addr: %s\n", conn.RemoteAddr().String())
 	//remoteAddr := strings.Split(conn.RemoteAddr().String(), ":")
 	//ConnectToClient(remoteAddr[0], 8005, p)
@@ -154,15 +205,18 @@ func KeepListening(ClientIP string, ClientPort int) (receivedPacket Type.Packet)
 
 }
 
+// 监听client的连接，当某个client连接上之后，处理其连接，等待请求的表的信息，获取表名之后将对应的Region地址返回给client
+// 如果是创建新表，需要选择一个region，通知region新建表项
+// 对于不同的
 func main() {
 	initMaster()
 	//reader, err := os.Open("1.txt")
 	//if err != nil {
 	//	log.Fatal(err)
 	//}
-	StatementChannel := make(chan types.DStatements, 500)
-	FinishChannel := make(chan string, 500)
-	TablesChannel := make(chan []string, 500)
+	StatementChannel = make(chan types.DStatements, 500)
+	FinishChannel = make(chan string, 500)
+	TablesChannel = make(chan []string, 500)
 	//FlushChannel := make(chan struct{})
 	go Parse2Statement(StatementChannel, FinishChannel, TablesChannel)
 	//fmt.Println("Initialized Master server")
@@ -181,7 +235,7 @@ func main() {
 
 	}
 	for {
-		client := KeepListening(ClientIP, 9000)
+		client := HandleClient(ClientIP, 9000)
 		fmt.Println(client)
 
 		//ConnectToRegion("127.0.0.1", 8006, p)
