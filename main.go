5623e08221ce7994d9904ed9be852a0a2dc47c6f
OE-Heart <leoouyixin@126.com>
2022-05-22 16:29:28 +0800

    feat: Add UpdateKey method in ServiceRegister

--- main.go
+++ main.go
@@ -6,18 +6,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net"
 	"os/exec"
+	"strings"
 )
 
 func runEtcd() {
-	cmd := exec.Command("etcd", "--name", "infra1", "--initial-advertise-peer-urls", "http://192.168.84.244:2380",
-		"--listen-peer-urls http://192.168.84.244:2380",
-		"--listen-client-urls http://192.168.84.244:2379,http://127.0.0.1:2379",
-		"--advertise-client-urls http://192.168.84.244:2379",
-		"--initial-cluster-token etcd-cluster-1",
-		"--initial-cluster infra0=http://192.168.84.48:2380,infra1=http://192.168.84.244:2380",
-		"--initial-cluster-state new")
-	// cmd := exec.Command("etcd", "--version")
+	// cmd := exec.Command("etcd", "--name", "infra1", "--initial-advertise-peer-urls", "http://192.168.84.244:2380",
+	// 	"--listen-peer-urls", "http://192.168.84.244:2380",
+	// 	"--listen-client-urls", "http://192.168.84.244:2379,http://127.0.0.1:2379",
+	// 	"--advertise-client-urls", "http://192.168.84.244:2379",
+	// 	"--initial-cluster-token", "etcd-cluster-1",
+	// 	"--initial-cluster", "infra0=http://192.168.84.48:2380,infra1=http://192.168.84.244:2380",
+	// 	"--initial-cluster-state", "new")
+	cmd := exec.Command("etcd")
 	stout, err := cmd.StdoutPipe()
 	if err != nil {
 		log.Fatalln(err)
@@ -35,22 +37,49 @@ func runEtcd() {
 
 }
 
-// func main() {
-// 	// runEtcd()
-// 	var endpoints = []string{"127.0.0.1:2379"}
-// 	ser, err := Region.NewServiceRegister(endpoints, "/web/node1", "localhost:8000", 5)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	//监听续租相应chan
-// 	go ser.ListenLeaseRespChan()
-// 	select {
-// 	// case <-time.After(20 * time.Second):
-// 	// 	ser.Close()
-// 	}
-// }
+// getLocalIpV4 获取 IPV4 IP，没有则返回空
+func getLocalIpV4(interfaceName string) (addr string, err error) {
+	inter, err := net.InterfaceByName(interfaceName)
+	if err != nil {
+		panic(err)
+	}
+	// 判断网卡是否开启，过滤本地环回接口
+	if inter.Flags&net.FlagUp != 0 && !strings.HasPrefix(inter.Name, "lo") {
+		// 获取网卡下所有的地址
+		addrs, err := inter.Addrs()
+		if err != nil {
+			panic(err)
+		}
+		for _, addr := range addrs {
+			if ipnet, ok := addr.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+				//判断是否存在IPV4 IP 如果没有过滤
+				if ipnet.IP.To4() != nil {
+					return ipnet.IP.String(), nil
+				}
+			}
+		}
+	}
+	return "", fmt.Errorf("interface %s don't have an ipv4 address", interfaceName)
+}
 
 func main() {
+	ip, err := getLocalIpV4("wifi0")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// runEtcd()
+	var endpoints = []string{"127.0.0.1:2379"}
+	prefix := "/region"
+	key := strings.Join([]string{prefix, ip}, "/")
+	fmt.Println(key)
+	ser, err := Region.NewServiceRegister(endpoints, key, "0", 5)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//监听续租相应chan
+	go ser.ListenLeaseRespChan()
+
 	MiniSQL.InitDB()
 	Region.InitRegionServer()
 }
@@ -63,19 +92,5 @@ func main() {
 // 	fmt.Println("bye")
 // 	if err != nil {
 // 		fmt.Println(err)
-// 	}
-// }
-
-// func main() {
-// 	var endpoints = []string{"127.0.0.1:2379"}
-// 	ser, err := Region.NewServiceRegister(endpoints, "/web/node1", "localhost:8000", 5)
-// 	if err != nil {
-// 		log.Fatalln(err)
-// 	}
-// 	//监听续租相应chan
-// 	go ser.ListenLeaseRespChan()
-// 	select {
-// 	// case <-time.After(20 * time.Second):
-// 	// 	ser.Close()
 // 	}
 // }
